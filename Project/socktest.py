146b438a2809439c4e01fbff130e9eda74e6baf9
Varun Shijo <varunshi@buffalo.edu>
2018-05-03 02:58:18 -0400

    cleaned

--- Project/socktest.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect(("pranavs-mbp", 5000))
-
-for i in range(1000):
-    client_socket.send(str(i))
-
-client_socket.close()
